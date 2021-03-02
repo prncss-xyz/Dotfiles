7bb36d6655d420e3e3e2689bd0c8b93b41d621e2
Juliette Lamarche <juliette.lamarche@princesse.xyz>
2021-03-02 14:19:33 -0500

    refactored

--- home/.config/fish/functions/n.fish
+++ home/.config/fish/functions/n.fish
@@ -27,7 +27,7 @@ function n --wraps nnn --description 'support nnn quit and change directory'
     # stty lwrap undef
     # stty lnext undef
 
-    nnn -c $argv
+    nnn $argv
 
     if test -e $NNN_TMPFILE
         source $NNN_TMPFILE

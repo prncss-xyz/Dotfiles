cfcd8ca8dfc0816999f20ee54f03246f225f7f41
Juliette Lamarche <juliette.lamarche@princesse.xyz>
2021-03-25 21:55:28 -0400

    opener script for nnn and more

--- all/.config/fish/config.fish
+++ all/.config/fish/config.fish
@@ -6,12 +6,12 @@ abbr -g ytdl 'youtube-dl -x -o "~/Media/Music/ytdl/%(artist)s %(title)s.(ext)s"'
 abbr -g gncc 'commit -a --allow-empty-message -m ""'
 abbr -g kdiff kitty +kitten diff
 
+alias o opener
 alias bat 'bat --style=changes,header,rule,snip'
 alias ls lsd
 alias wttr 'curl "fr.wttr.in/montreal?n"'
 alias get_tree 'swaymsg -t get_tree > /tmp/tree.json && browser /tmp/tree.json'
 alias cp-last 'history|head -1|wl-copy'
-alias imgs 'swaymsg -t get_tree | jq \'recurse(.nodes[])|.name\' | sed -n \'s/imv - .* \(\/.*\) \[.*\] /\1/p\''
 alias plopg 'plop --plopfile="$HOME/Media/Projects/plopg/plopfile.js" --dest=.'
 function pnpm
   command pnpm $argv|lolcat
@@ -26,3 +26,5 @@ end
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"

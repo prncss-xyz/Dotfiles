ac561f228902ec528e3508f99b1697e86cdea6f4
Juliette Lamarche <juliette.lamarche@princesse.xyz>
2021-07-14 17:08:58 -0400

    renamed perso

---
+++ personal/post-install/perso.sh
@@ -0,0 +1,9 @@
+git config --global user.name "Juliette Lamarche"
+git config --global user.email "juliette.lamarche@princesse.xyz"
+git config --global user.signingkey DB6F754B59A68BC3AE6CF695004A3B1CB2774189
+
+pnpm config set init-author-name "Juliette Lamarche"
+pnpm config set init-author-email "juliette.lamarche@princesse.xyz"
+pnpm config set init-author-url "https://github.com/prncss-xyz/"
+pnpm config set init-license MIT
+

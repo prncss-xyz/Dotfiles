c36b9e1be0e7e6135ce3fcf884eafb0c29d69e64
Juliette Lamarche <juliette.lamarche@princesse.xyz>
2021-08-04 21:50:29 -0400

    anywise_reg compat refactor

--- main/.config/nvim/lua/setup/anywise_reg.lua
+++ main/.config/nvim/lua/setup/anywise_reg.lua
@@ -1,3 +1,5 @@
+local ac = require'anywise_compat'
+
 -- anywise-reg + cutlass
 local function map(modes, lhs, rhs, opts)
   local options = { noremap = true }
@@ -13,64 +15,12 @@ local function map(modes, lhs, rhs, opts)
   end
 end
 
-local function format_str_args(str_args)
-  local args_str = ''
-  for _, str_arg in ipairs(str_args) do
-    args_str = args_str .. [[']] .. str_arg .. [[', ]]
-  end
-  return args_str:sub(1, -3)
-end
-
-local function set_keymap(lhs, rhs)
-  local opts = { noremap = false }
-  local mode = 'n'
-  vim.api.nvim_buf_set_keymap(0, mode, lhs, rhs, opts)
-end
-
-local function product(textobjects)
-  if textobjects[1] == nil then
-    return { '' }
-  end
-  local textobject = table.remove(textobjects, 1)
-  local expanded_textobjects = product(textobjects)
-  local new_expanded_textobjects = {}
-  for _, start in ipairs(textobject) do
-    for _, rest in ipairs(expanded_textobjects) do
-      table.insert(new_expanded_textobjects, start .. rest)
-    end
-  end
-  return new_expanded_textobjects
-end
-
-local function expand_textobjects(textobjects)
-  for i, textobject in ipairs(textobjects) do
-    if type(textobject) == 'string' then
-      textobjects[i] = { textobject }
-    end
-  end
-  return product(textobjects)
-end
-
 local textobjects = {
   { 'i', 'a' },
   { 'w', 'W' },
 }
 
-local to = expand_textobjects(textobjects)
-function _G.setup_yank_keymap(opl, prefix, opr)
-  for _, textobject in ipairs(to) do
-    local lhs = opl .. textobject
-    local rhs = '<Cmd>lua require("anywise_reg.keybinds").perform_action('
-      .. format_str_args { prefix, opr, textobject }
-      .. ')<CR>'
-    set_keymap(lhs, rhs)
-  end
-end
-
-function _G.setmaps()
-  setup_yank_keymap('x', '"+', 'd')
-  setup_yank_keymap('X', '"+', 'c')
-end
+ac.setup(textobjects)
 
 require('anywise_reg').setup {
   operators = { 'y' },
@@ -81,8 +31,9 @@ require('anywise_reg').setup {
   register_print_cmd = true,
 }
 
-vim.cmd 'autocmd BufEnter * :lua _G.setmaps()'
 
+ac.setmap('x', '"+', 'd')
+ac.setmap('X', '"+', 'c')
 -- cutlass-inspired
 -- select mode missing
 map('nx', 'x', '"+d')

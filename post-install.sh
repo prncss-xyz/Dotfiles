1d38a90c437ce58fef87f73828800a8031b1cc77
Juliette Lamarche <juliette.lamarche@princesse.xyz>
2021-09-09 23:57:16 -0400

    various

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=sh
 cd ~/Dotfiles/stow || exit 1
 stow stow host-"$(hostname)" main perso bindings-qwerty
-
+sudo pacman -Syu
 dir=$(mktemp -d)
 cd "$dir" || exit 1
 git clone --depth=1 https://aur.archlinux.org/yay.git
@@ -9,4 +9,4 @@ cd yay || exit 1
 makepkg -si
 cat ~/.config/stow/arch/* | xargs yay --batchinstall --needed --noconfirm -S
 rm -rf "$dir"
-cat ~/.config/stow/post-install/* | sh
+cat ~/.config/stow/post-install/*|sh

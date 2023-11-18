1e685b3c01aaba1631d472e4f67e281042c97d86
Juliette Lamarche <princesse@princesse.xyz>
2023-11-18 08:16:51 -0500

    multus

--- main/.config/stow/post-install/systemd.sh
+++ main/.config/stow/post-install/systemd.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env sh
 
 sudo systemctl enable bluetooth
+sudo systemctl enable gdm
 sudo systemctl enable iwd
 sudo systemctl enable sshd
+sudo systemctl enable networkd
+sudo systemctl enable resolved
 
 systemctl --user enable foot-server
+systemctl --user enable river-tag-overlay
+systemctl --user enable swaybg
+systemctl --user enable swayidle
+systemctl --user enable way-displays
 systemctl --user enable wlsunset
 systemctl --user enable udiskie
 systemctl --user enable waybar
 systemctl --user enable syncthing
 systemctl --user enable mako
-systemctl --user enable flashfocus
-# systemctl --user enable kdeconnect-sms
-# systemctl --user enable kdeconnect-app
 systemctl --user enable sway-mega-hotkeys
